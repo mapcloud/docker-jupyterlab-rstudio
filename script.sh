8a45648067ee8568c8820a2221048a2476b8362a
Benjamin Du <duchuanlong+git@gmail.com>
2017-05-07 10:07:08 +0800

    start jupyterlab but not notebook

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 
 Rscript -e 'IRkernel::installspec()'
 /usr/lib/rstudio-server/bin/rserver --server-daemonize 0 &
-jupyter notebook --ip=* --port=8888 --no-browser --notebook-dir=/jupyter &
-jupyter lab --ip=* --port=8889 --no-browser --notebook-dir=/jupyter 
+# jupyter notebook --ip=* --port=8888 --no-browser --notebook-dir=/jupyter &
+jupyter lab --ip=* --port=8888 --no-browser --notebook-dir=/jupyter 
